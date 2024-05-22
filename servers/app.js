6f38eaa02cdb20712cbe37f50d1cd2469aca2bd7
Felex Kuria <engineerfelex@gmail.com>
2024-05-22 15:59:13 +0300

    Moved ejs

--- servers/app.js
+++ servers/app.js
@@ -2,20 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+// register view engine
+app.set("view engine",'ejs')
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index')
+    //res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    res.render('about')
+
+   // res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
-app.get('/about-us', (req, res) => {
-    res.sendFile(path.join(__dirname,"views","about.html"))
+app.get('/blog/create', (req, res) => {
+   res.render('create')
+    // res.sendFile(path.join (__dirname,"views","about.html"))
 });
 // Handle 404 errors
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render("404")
+   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 // Start the server
 app.listen(PORT, () => {
